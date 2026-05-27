55049b85d2b881582305ce0e4990f360ae2cbbe3
Luca Fueyo <lucafueyo05@gmail.com>
2026-05-27 14:30:41 -0300

    conectando riderApp con paymentApp

--- lib/actions/distribuirFormulario.ts
+++ lib/actions/distribuirFormulario.ts
@@ -5,7 +5,5 @@ import { redirect } from "next/navigation";
 
 export async function distribuirFormulario(formData: FormData): Promise<void> {
   const result = await nuevoTrabajo(formData);  
-  const id_viaje = result.id_viaje;
-  const id_cliente = result.id_cliente;
-  redirect(`/mocks/payment?id_viaje=${id_viaje}&id_cliente=${id_cliente}`)
+ // proximamente se hara los fetch a driverApp y feedbackApp
 }
